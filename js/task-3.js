f2d3988c91b81c340faa8f84ac345b95793826df
Stanislave <stasdroner@gmail.com>
2026-03-29 10:04:48 +0300

    Add task 3

---
+++ js/task-3.js
@@ -0,0 +1,36 @@
+class StringBuilder {
+  #value;
+
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+
+  getValue() {
+    return this.#value;
+  }
+
+  padEnd(str) {
+    this.#value += str;
+  }
+
+  padStart(str) {
+    this.#value = [].concat(str.split(''), this.#value.split('')).join('');
+  }
+
+  padBoth(str) {
+    this.#value = [
+      ...str.split(''),
+      ...this.#value.split(),
+      str.split(''),
+    ].join('');
+  }
+}
+
+const builder = new StringBuilder('.');
+console.log(builder.getValue()); // "."
+builder.padStart('^');
+console.log(builder.getValue()); // "^."
+builder.padEnd('^');
+console.log(builder.getValue()); // "^.^"
+builder.padBoth('=');
+console.log(builder.getValue()); // "=^.^="
